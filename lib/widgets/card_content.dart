d4df1b4a7ce3e157529f1b7b002856c51720c25d
Gurupratap <gurupratap.matharu@gmail.com>
2019-11-25 14:49:22 -0300

    card content segregated

--- lib/widgets/card_content.dart
+++ lib/widgets/card_content.dart
@@ -8,18 +8,54 @@ class CardContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(name),
-        SizedBox(
-          height: 8,
-        ),
-        Text(
-          date,
-        ),
-        Spacer(),
-        Row(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            name,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            date,
+            style: TextStyle(color: Colors.grey),
+          ),
+          Spacer(),
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                color: Color(0xFF162A49),
+                child: Text('Reserve'),
+                textColor: Colors.white,
+                elevation: 8,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32),
+                ),
+                onPressed: () {
+                  print("I am pressed!");
+                },
+              ),
+              Spacer(),
+              Text(
+                '0.00 \$',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
