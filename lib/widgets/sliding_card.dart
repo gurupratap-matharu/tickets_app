0016a447a66d95dedf936120865d0fdeaad21fc6
Gurupratap <gurupratap.matharu@gmail.com>
2019-11-26 09:51:09 -0300

    added new transition effect

--- lib/widgets/sliding_card.dart
+++ lib/widgets/sliding_card.dart
@@ -1,40 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:tickets_app/widgets/card_content.dart';
+import 'dart:math' as math;
 
 class SlidingCard extends StatelessWidget {
-  SlidingCard(
-      {@required this.name, @required this.date, @required this.assetName});
+  SlidingCard({
+    @required this.name,
+    @required this.date,
+    @required this.assetName,
+    this.offset,
+  });
 
   final String name;
   final String date;
   final String assetName;
+  final double offset;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.only(left: 8, right: 8, bottom: 24),
-      elevation: 8,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
-      child: Column(
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
-            child: Image.asset(
-              'assets/$assetName',
+    double gauss = math.exp(-(math.pow((offset.abs() - 0.5), 2) / 0.08));
+    return Transform.translate(
+      offset: Offset(-32 * gauss * offset.sign, 0),
+      child: Card(
+        margin: EdgeInsets.only(left: 8, right: 8, bottom: 24),
+        elevation: 8,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
+              child: Image.asset(
+                'assets/$assetName',
 //              height: MediaQuery.of(context).size.height * 0.3,
-              fit: BoxFit.none,
+                alignment: Alignment(-offset.abs(), 0),
+                fit: BoxFit.none,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Expanded(
-            child: CardContent(
-              name: name,
-              date: date,
+            SizedBox(
+              height: 8,
             ),
-          ),
-        ],
+            Expanded(
+              child: CardContent(
+                name: name,
+                date: date,
+                offset: gauss,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
